449e6f6d170a4e5eb9b742897b3a418f1ab833e2
Hassan Ali <hassanali1.dev@gmail.com>
2024-05-18 11:43:01 +0500

    App Name Changed.

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'riilu',
+      title: 'Riilu',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
         useMaterial3: true,
